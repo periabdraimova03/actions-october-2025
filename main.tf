bbef01c74705a4d80786bc2faaaf419b3c1257e1
periabdraimova03 <peri.abdraimova@gmail.com>
2026-02-11 19:36:56 -0600

    added buckend

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource aws_instance hello {
     ami = "ami-0c1fe732b5494dc14"
     instance_type = "t3.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-perizata"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
